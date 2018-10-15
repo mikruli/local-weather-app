4243a65ca8ffb2cd55567fb0e541f6697e074ff8
mikruli <kumricdejan@gmail.com>
2018-10-16 01:45:54 +0200

    dodata prva komponenta

---
+++ src/app/current-weather/current-weather.component.ts
@@ -0,0 +1,27 @@
+import { ICurrentWeather } from './../interfaces';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-current-weather',
+  templateUrl: './current-weather.component.html',
+  styleUrls: ['./current-weather.component.css']
+})
+export class CurrentWeatherComponent implements OnInit {
+
+  current: ICurrentWeather;
+
+  constructor() {
+      this.current = {
+        city: 'Bethesda',
+        country: 'US',
+        date: new Date(),
+        image: './assets/img/sunny.svg',
+        temperature: 72,
+        description: 'sunny'
+      } as ICurrentWeather;
+   }
+
+  ngOnInit() {
+  }
+
+}
