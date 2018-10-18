7d78523055c182da152798bea6c8c2a97f6bd7f7
mikruli <kumricdejan@gmail.com>
2018-10-18 22:13:48 +0200

    display current location weather added

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -1,3 +1,4 @@
+import { WeatherService } from './../weather/weather.service';
 import { ICurrentWeather } from './../interfaces';
 import { Component, OnInit } from '@angular/core';
 
@@ -10,18 +11,11 @@ export class CurrentWeatherComponent implements OnInit {
 
   current: ICurrentWeather;
 
-  constructor() {
-      this.current = {
-        city: 'Bethesda',
-        country: 'US',
-        date: new Date(),
-        image: './assets/img/sunny.svg',
-        temperature: 72,
-        description: 'sunny'
-      } as ICurrentWeather;
+  constructor(private weatherService: WeatherService) {
    }
 
   ngOnInit() {
+    this.weatherService.getCurrentWeather('Bethesda', 'US').subscribe((data) => this.current = data);
   }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  appId: '86ad863ebf36507152a155e4a99eca0f',
+  baseUrl: 'http://',
 };
 
 /*
